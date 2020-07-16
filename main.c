d6adbe5302a9dc20341999cff317945d20aafcf2
Shahaf <shahaf1313@gmail.com>
2020-07-16 17:36:26 +0300

    Timer
    Creating a timer in the program

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
+/* This function makes a delay of ms milliseconds */
+void delay(int ms)
+{
+    int clocks = CLOCKS_PER_SEC * ms / 1000;
+    clock_t stopTime = clocks + clock();
+    while (stopTime > clock());
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int i = 0;
+    while(i <= 3)
+    {
+        printf("Iteration %d\n", i);
+        ++i;
+        delay(SLEEP_TIME_SEC * 1000);
+    }
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MONEYTOR_MAIN_H
+#define MONEYTOR_MAIN_H
+
+#include <stdio.h>
+#include <time.h>
+#define SLEEP_TIME_SEC 10
+
+/* This function makes a delay of ms milliseconds */
+void delay(int ms);
+
+#endif //MONEYTOR_MAIN_H
